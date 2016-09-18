aed2f1110a4c67a4a69a83cf7657088a128c6ff7
roblievens <mail@roblievens.be>
2016-09-18 15:33:18 +0200

    Commit 01 - Layout

--- AardsMooi/App_Start/BundleConfig.cs
+++ AardsMooi/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace AardsMooi
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/aardsmooi.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -25,7 +26,10 @@ namespace AardsMooi
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/aardsmooi.css",
+                      "~/Content/navbar.css",
+                       "~/Content/font-awesome.css"));
         }
     }
 }
